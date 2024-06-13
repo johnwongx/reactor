93e44bec52f5510a701cccdf56a6bcf57a11bce8
JhonWong <1290773033@qq.com>
2024-06-13 08:41:20 +0800

    添加注释与格式文件

--- Socket.h
+++ Socket.h
@@ -22,7 +22,16 @@ class Socket {
   Socket(int fd);
   ~Socket();
 
+  // 快速重启：当一个应用程序关闭后，它占用的端口号可能会处于 TIME_WAIT 状态，
+  // 而 TIME_WAIT 可以持续几分钟。
+  // 在这个状态下，默认情况下，同一个端口号不能被再次使用。
+  // 启用 SO_REUSEADDR 后，可以立即重启服务器监听同一个端口，而不需要等待
+  // TIME_WAIT 状态结束。
+  // 也允许多个套接字绑定到同一个端口
   void setReuseAddr(bool on);
+
+  // 用于控制 TCP Nagle
+  // 算法的启用或禁用。Nagle算法旨在减少小数据包在网络中的流量，它通过合并一些小的TCP包来减少网络上的负载。
   void setNoDelay(bool on);
 
   void bind(const InetAddress& addr);
